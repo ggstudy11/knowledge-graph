72a6f7881a4b04607cfbfe306cc0b1291c27de5d
ggstudy11 <439403849@qq.com>
2025-04-28 18:38:09 +0800

    add bat to deploy and file-service online now

---
+++ deploy.bat
@@ -0,0 +1,19 @@
+@echo off
+
+:: 构建镜像
+echo Building Docker images...
+docker-compose build
+
+:: 推送镜像到 Docker Hub
+echo Pushing gateway-service to Docker Hub...
+docker push ggstudy11/kg-gateway:latest
+
+echo Pushing user-service to Docker Hub...
+docker push ggstudy11/kg-userservice:latest
+
+echo Pushing file-service to Docker Hub...
+docker push ggstudy11/kg-fileservice:latest
+
+echo All images have been successfully pushed to Docker Hub!
+
+pause
